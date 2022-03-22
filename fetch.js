29964831b7b46eccf1cfc2e6ec333a6b5a9109d7
Rose Kamal Love <rosekamallove@gmail.com>
2022-03-23 00:53:11 +0530

    button increment, reFetch

--- fetch.js
+++ fetch.js
@@ -11,24 +11,36 @@ class FetchExample extends PolymerElement {
     this.num = 1;
     this.fetchedData = null;
   }
-
   connectedCallback() {
     super.connectedCallback();
     fetch(`https://reqres.in/api/products/${this.num}`).then((data) =>
       data
         .json()
-        .then((res) => (this.fetchedData = JSON.stringify(res.data, null, 2)))
+        .then((res) => (this.fetchedData = JSON.stringify(res, null, 2)))
     );
+  }
+
+  increment() {
+    this.num = this.num + 1;
+    this.connectedCallback();
   }
 
   static get template() {
     return html`
+      <style>
+        .pre {
+          padding: 10px;
+          margin: 10px;
+          border-radius: 5px;
+          background-color: #e8e8e8;
+        }
+      </style>
       <h1>Fetched number: [[num]]</h1>
-      Fetched Result:
-      <pre>
-    [[fetchedData]]
-        </pre
-      >
+      <h3>Fetched Response:</h3>
+      <div class="pre">
+        <pre>[[fetchedData]]</pre>
+      </div>
+      <button on-click="increment">Increment</button>
     `;
   }
 }
